d95ae0acb97be1ad2fe4b4e7ca612e763155de4a
jshrake <justinshrake@gmail.com>
2016-09-06 21:06:56 -0700

    Use GL_NEAREST for min/mag filter

--- grim_surface.h
+++ grim_surface.h
@@ -200,8 +200,8 @@ void grim_surface_begin(grim_surface *surface) {
   glBindTexture(GL_TEXTURE_2D, surface->texture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 
 void grim_surface_render(grim_surface *) {
